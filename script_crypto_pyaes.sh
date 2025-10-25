f21f9d91f8fb9060f5c0d5ef13b288bf3a41c4b0
Yoav Gold <yoav9113@gmail.com>
2025-10-26 00:32:36 +0300

    script_crypto_pyaes.sh

--- script_crypto_pyaes.sh
+++ script_crypto_pyaes.sh
@@ -23,7 +23,7 @@ source "$VENV_DIR/bin/activate"
 # Install dependencies
 echo "[INFO] Installing required packages..."
 pip install -U pip
-pip install numba numpy plotly pyinstrument pyperf pyperformance py-spy pyaes pandas
+pip install numba numpy plotly pyinstrument pyperf pyperformance py-spy pyaes pandas openpyxl
 
 # Run benchmarks
 LOG_DIR="results/aes"
